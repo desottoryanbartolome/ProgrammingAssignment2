f7f1a4829d92ab719500cd34d1602d6b82f78bfc
desottoryanbartolome <DESOTTORYANBARTOLOME@YAHOO.COM>
2015-08-23 22:46:06 +0900

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+##This are the functions I have made to cache the inverse of a matrix
 
-## Write a short comment describing this function
+##As required, this first function creates a special matrix object which can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+        m<-NULL
+        set<-function(y){
+  	        x<<-y
+ 	        m<<-NULL
+	}
+        get<-function() x
+        setinv<-function(solve) m<<- solve
+        getinv<-function() m
+        list(set=set, get=get, setinv=setinv, getinv=getinv)
+        }
 
-}
 
-
-## Write a short comment describing this function
+#This next function computes for the inverse of the matrix (note that the values of the matrix should be set/defined first.
+#should the matrix remain unchanged, the function will just recall the cached inverse.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+        m<-x$getinv()
+        if(!is.null(m)){
+                message("getting cached data")
+                return(m)
+        }
+        orig<-x$get()
+        m<-solve(orig, ...)
+        x$setinv(m)
+        m
+        }
+#start by placing the makeCacheMatrix function to a variable (e.g. mx)
+#set the matrix (e.g. mx$set(matrix(c(1,3.24,12.52,1.122,2,2))
+#to view the matrix use get() (e.g. mx$get())
+#Get the inverse of the matrix using cacheSolve (e.g. cacheSolve(mx))
+#repeating cacheSolve(mx) will yield to message stating that the matrix has not changed
+
+#Another check done was to do mx$set(mx$getinv())
+#when cacheSolve(mx) is executed, the new inverse matrix should result to the original matrix set.
